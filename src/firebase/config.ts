86bc18455a70371096a6a477c2b57b6e1bca4b74
Takumi <nase49946@gmail.com>
2022-02-05 15:35:10 +0900

    init

---
+++ src/firebase/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAAwliJWz3iElyUqL6E-27FRsny7nTDNhI",
+  authDomain: "menu-2d67e.firebaseapp.com",
+  databaseURL: "https://menu-2d67e.firebaseio.com",
+  projectId: "menu-2d67e",
+  storageBucket: "menu-2d67e.appspot.com",
+  messagingSenderId: "106080504750",
+  appId: "1:106080504750:web:48341353f945fd8857163f",
+  measurementId: "G-X093DM6V3H"
+};
