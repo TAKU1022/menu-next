73922743bec6964a59b345d13cefddbc15ea3d4a
Takumi <nase49946@gmail.com>
2022-02-06 23:17:35 +0900

    feat: firebase初期化

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAAwliJWz3iElyUqL6E-27FRsny7nTDNhI',
-  authDomain: 'menu-2d67e.firebaseapp.com',
-  databaseURL: 'https://menu-2d67e.firebaseio.com',
-  projectId: 'menu-2d67e',
-  storageBucket: 'menu-2d67e.appspot.com',
-  messagingSenderId: '106080504750',
-  appId: '1:106080504750:web:48341353f945fd8857163f',
-  measurementId: 'G-X093DM6V3H',
+  apiKey: 'AIzaSyAQ-Ar-fDVfJmCax6pP1Z7zGBxmycZOQ2o',
+  authDomain: 'menu-next.firebaseapp.com',
+  projectId: 'menu-next',
+  storageBucket: 'menu-next.appspot.com',
+  messagingSenderId: '165244273791',
+  appId: '1:165244273791:web:532598d82aee2c30852b4a',
+  measurementId: 'G-JT2LWWEWKT',
 };
