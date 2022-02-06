e1960f2e3f2a2e939b293e3ac6e9fb3b86d4e3a6
Takumi <nase49946@gmail.com>
2022-02-06 17:32:26 +0900

    feat: User型作成

---
+++ src/types/user.ts
@@ -0,0 +1,16 @@
+export type User = {
+  uid: string;
+  name: string;
+  avaterURL: string;
+  email: string;
+  createdAt: Date;
+  admin: boolean;
+  eatCount: number;
+  isEatenBreakfast: boolean;
+  isEatenLunch: boolean;
+  isEatenDinner: boolean;
+  isCompletedCreateMyMenuTutorial: boolean;
+  isCompletedHomeTutorial: boolean;
+  isCreatedMyMenu: boolean;
+  postCount: number;
+};
