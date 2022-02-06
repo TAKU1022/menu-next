aa1b138fb51251a7a904523a0ecad454d998e764
Takumi <nase49946@gmail.com>
2022-02-06 15:05:54 +0900

    feat: firebase設定追加

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,4 +1,9 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
 import { firebaseConfig } from './config';
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
