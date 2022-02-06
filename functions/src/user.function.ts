dceb826ddb2cea252f9919e666cad2fe73abb8d1
Takumi <nase49946@gmail.com>
2022-02-06 23:18:00 +0900

    feat: user作成時のトリガー関数作成

---
+++ functions/src/user.function.ts
@@ -0,0 +1,27 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const createUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate((user) => {
+    return db.doc(`users/${user.uid}`).set({
+      uid: user.uid,
+      name: user.displayName,
+      avaterURL: user.photoURL,
+      email: user.email,
+      createdAt: new Date(),
+      admin: false,
+      eatCount: 0,
+      isEatenBreakfast: false,
+      isEatenLunch: false,
+      isEatenDinner: false,
+      isCompletedCreateMyMenuTutorial: false,
+      isCompletedHomeTutorial: false,
+      isCreatedMyMenu: false,
+      postCount: 0,
+    });
+  });
