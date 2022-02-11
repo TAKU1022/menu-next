b0972d6f4e79dea44bbfcb21404e8b73ba8aa69b
Takumi <nase49946@gmail.com>
2022-02-11 23:29:43 +0900

    feat: 認証ガード作成

--- src/components/functional/AuthGuard.tsx
+++ src/components/functional/AuthGuard.tsx
@@ -1,9 +1,18 @@
-import { ReactNode, VFC } from 'react';
+import { ReactNode, useEffect, VFC } from 'react';
+import { useUser } from 'src/hooks/useUser';
 
 type Props = {
   children: ReactNode;
 };
 
 export const AuthGuard: VFC<Props> = ({ children }) => {
+  const { userState, listenUserState } = useUser();
+
+  useEffect(() => {
+    if (!userState) {
+      listenUserState();
+    }
+  }, [userState, listenUserState]);
+
   return <>{children}</>;
 };
