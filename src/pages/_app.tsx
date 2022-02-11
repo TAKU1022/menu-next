0f49ff446a9857842c3aa23768200d669fbb9a61
Takumi <nase49946@gmail.com>
2022-02-11 23:28:55 +0900

    feat: グローバルcss追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { UserProvider } from '../components/context/user/UserProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = extendTheme({
+    styles: {
+      global: {
+        'html, body': {
+          height: '100%',
+        },
+      },
+    },
+  });
+
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <UserProvider>
         <Component {...pageProps} />
       </UserProvider>
