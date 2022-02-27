5485f56fea021aae1e8be49e512c12b6dcfdd4fc
Takumi <nase49946@gmail.com>
2022-02-27 12:20:18 +0900

    feat: ページ遷移ローディングUI追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { ChakraProvider, extendTheme, Progress } from '@chakra-ui/react';
 import { UserProvider } from '../components/context/user/UserProvider';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [isLoadingPage, updateIsLoadingPage] = useState<boolean>(false);
+
   const theme = extendTheme({
     styles: {
       global: {
@@ -18,9 +23,40 @@ function MyApp({ Component, pageProps }: AppProps) {
     },
   });
 
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      url !== router.asPath && updateIsLoadingPage(true);
+    };
+    const handleComplete = () => {
+      updateIsLoadingPage(false);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router]);
+
   return (
     <ChakraProvider theme={theme}>
       <UserProvider>
+        {isLoadingPage && (
+          <Progress
+            size="xs"
+            colorScheme="messenger"
+            isIndeterminate
+            width={'100%'}
+            backgroundColor="transparent"
+            position="fixed"
+            top={0}
+            zIndex={999}
+          />
+        )}
         <Component {...pageProps} />
       </UserProvider>
     </ChakraProvider>
