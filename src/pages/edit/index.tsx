72ad66dbdcc868dd882fcb7bf3a258ed60bba63b
Takumi <nase49946@gmail.com>
2022-03-19 23:41:58 +0900

    feat: editd作成

---
+++ src/pages/edit/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { CommonLayout } from 'src/components/layout/CommonLayout';
+
+const Edit: NextPage = () => {
+  return (
+    <CommonLayout>
+      <div>
+        <h2>hhh</h2>
+      </div>
+    </CommonLayout>
+  );
+};
